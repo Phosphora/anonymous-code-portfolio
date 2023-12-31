8cf19b8b75500a95042e9df4cc7e291ac82775e2
Winnie Li <winnieli535@gmail.com>
2023-12-30 20:46:16 -0500

    Début du texte de la page À propos de moi

--- src/code/textes.js
+++ src/code/textes.js
@@ -8,7 +8,7 @@ export const texteAccueil = {
       },
       section2: {
         titre: "Explorez la créativité, vivez l'innovation",
-        p: "Bonjour et bienvenue dans mon espace numérique, où la créativité rencontre la fonctionnalité, et l'innovation le design ! Je suis Winnie Li, une développeuse web passionnée et dynamique avec un intérêt marqué pour la création d'expériences numériques immersives. Si vous souhaitez en savoir plus sur moi, veuillez consulter la section de mon site web.",
+        p: "Bonjour et bienvenue dans mon espace numérique, où la créativité rencontre la fonctionnalité, et l'innovation le design ! Je suis Winnie Li, une étudiante en multimédia. Si vous souhaitez en savoir plus sur moi, veuillez consulter la section de mon site web.",
         lien: "À propos de moi"
       },
       section3: {
@@ -27,7 +27,7 @@ export const texteAccueil = {
       },
       section2: {
         titre: "Explore Creativity, Experience Innovation",
-        p: "Hello and welcome to my digital space, where creativity meets functionality, and innovation meets design! I'm Winnie Li, a passionate and dynamic web developer with a keen interest in creating immersive digital experiences. If you'd like to know more about me, please visit the section of my website.",
+        p: "Hello and welcome to my digital space, where creativity meets functionality, and innovation meets design! I'm Winnie Li, a multimedia student. If you'd like to know more about me, please visit the section of my website.",
         lien: "About me"
       },
       section3: {
@@ -41,313 +41,134 @@ export const texteAccueil = {
     }
   };
   
-  // Header
-  export const texteHeader = {
-    fr: {
-      sectionNavigation: {
-        accueil: "Accueil",
-        monProfil: "À propos de moi",
-        mesProjets: "Portfolio",
-        contact: "Contact"
-      }
-      
-    },
+// Header
+export const texteHeader = {
+  fr: {
+    sectionNavigation: {
+      accueil: "Accueil",
+      monProfil: "À propos de moi",
+      mesProjets: "Portfolio",
+      contact: "Contact"
+    }
+  },
   
-    en: {
-      sectionNavigation: {
-        accueil: "Home",
-        monProfil: "About me",
-        mesProjets: "Portfolio",
-        contact: "Contact"
-      }
-      
+  en: {
+    sectionNavigation: {
+      accueil: "Home",
+      monProfil: "About me",
+      mesProjets: "Portfolio",
+      contact: "Contact"
     }
   }
+}
   
-  // Footer
-  export const texteFooter = {
-    fr: {
-      titre: "Prenez contact",
-      p: "Je suis toujours ouverte à la collaboration et aux nouvelles opportunités. N'hésitez pas à me contacter."
+// Footer
+export const texteFooter = {
+  fr: {
+    titre: "Prenez contact",
+    p: "Je suis toujours ouverte à la collaboration et aux nouvelles opportunités. N'hésitez pas à me contacter."
+  },
+  
+  en: {
+    titre: "Get in Touch",
+    p: "I'm always open to collaboration and new opportunities. Feel free to reach out."
+  }
+}
+  
+// Bouton Filtrage
+export const texteFiltre = {
+  fr: {
+    typeProjet: "Type de projets : "
+  },
+  
+  en: {
+    typeProjet: "Project Type : "
+  }
+}
+  
+// Page À propos de moi
+export const texteMonProfil = {
+  fr: {
+    section1: {
+      titre: "À propos de moi",
+      nom: "Nom : Winnie Li",
+      alias: "Alias : Phosphora",
+      age: "Âge : 19 ans"
+    },
+    section2: {
+      titre: "Ce que j'apporte à la table :"
+    },
+    section3: {},
+    section4: {}
+  },
+  
+  en: {
+    section1: {
+      titre: "About me",
+      nom: "Name : Winnie Li",
+      alias: "Alias : Phosphora",
+      age: "Age : 19 years old"
+    },
+    section2: {
+      titre: "What I Bring to the Table:"
+    },
+    section3: {},
+    section4: {}
+  }
+}
+  
+// Page Portfolio
+export const textePortfolio = {
+  fr: {
+    section1: {
+      titre1: "Projets étudiants",
+      titre2: "Décrouvez l'incroyable créativité de nos étudiants à travers une vitrine de",
+      span: "projets captivants.",
+      titre3: "Explorez des réalisations exceptionnelles et plongez dans l'univers du TIM."
     },
   
-    en: {
-      titre: "Get in Touch",
-      p: "I'm always open to collaboration and new opportunities. Feel free to reach out."
-    }
-  }
-  
-  // Bouton Filtrage
-  export const texteFiltre = {
-  
-    fr: {
-      session: "Session : ",
-      typeCours: "Type de cours : ",
-      typeProjet: "Type de projets : "
-      // accueil: "Accueil",
-      // cours: "Cours",
-      // projets: "Projets Étudiants",
-      // prof: "Enseignants",
-      // activite: "Activités",
-      // futur: "Futur"
-    },
-  
-    en: {
-      session: "Session : ",
-      typeCours: "Course Type : ",
-      typeProjet: "Project Type : "
-      // accueil: "Home",
-      // cours: "Classes",
-      // projets: "Student Projects",
-      // prof: "Teachers",
-      // activite: "Activities",
-      // futur: "Future"
-    }
-  
-  }
-  
-  // Page des cours
-  export const texteCours = {
-  
-    fr: {
-      section1: {
-        titre1: "Cours",
-        titre2: "Plongez dans un monde dynamique de possibilités créatives et techniques avec",
-        span: "nos\u00A0cours."
-      },
-  
-      section2: {
-          tab: {
-            tab1: "Tout",
-            tab2: "Formation spécifique",
-            tab3: "Formation générale",
-          },
-          filtre: {
-            filtre1: "Session",
-            filtre2: "Cours",
-          },
-          filtreOption: {
-            filtreOption1: "Tout",
-            filtreOption2: "Jeux",
-            filtreOption3: "Web",
-            filtreOption4: "Création",
-            filtreOption5: "Conception",
-            filtreOption6: "Domaine Professionnel"
-          },
-          p1: "Résultats"
-        },
-  
-        section3: {
-          p1: "Visualiser le cheminement complet",
-        }
-      },
-  
-      en: {
-        section1: {
-          titre1: "Classes",
-          titre2: "Immerse yourself in a dynamic world of creative and technical possibilities with",
-          span: "our\u00A0courses."
-        },
-    
-        section2: {
-            tab: {
-              tab1: "All courses",
-              tab2: "Specific courses",
-              tab3: "Base courses",
-            },
-            filtre: {
-              filtre1: "Semesters",
-              filtre2: "Courses",
-            },
-            filtreOption: {
-              filtreOption1: "All",
-              filtreOption2: "Games",
-              filtreOption3: "Web",
-              filtreOption4: "Creation",
-              filtreOption5: "Design",
-              filtreOption6: "Professional field"
-            },
-            p1: "Results"
-          },
-  
-          section3: {
-            p1: "Visualize the full program",
-          }
-        },
-        
-  
-  }
-  
-  // Page des projets
-  export const texteProjet = {
-  
-    fr: {
-      section1: {
-        titre1: "Projets étudiants",
-        titre2: "Décrouvez l'incroyable créativité de nos étudiants à travers une vitrine de",
-        span1: "projets captivants.",
-        titre3: "Explorez des réalisations exceptionnelles et plongez dans l'univers du TIM."
-      },
-  
-      section2: {
-        p1: "Filtrer par : ",
-        filtreOption: {
-          filtreOption1: "Tout",
-          filtreOption2: "Jeux",
-          filtreOption3: "Web",
-          filtreOption4: "Création",
-          filtreOption5: "Conception",
-        },
-        
-        projetSeul: {
-          p1: "Réalisé par : ", 
-        }
-      },
-      
-    },
-  
-    en: {
-        section1: {
-          titre1: "Student projects",
-          titre2: "Discover the incredible creativity of our students through a showcase of",
-          span1: "captivating projects.",
-          titre3: "Explore exceptional achievements and immerse yourself in the world of TIM."
-        },
-  
-        section2: {
-          p1: "Filter by : ",
-          filtreOption: {
-            filtreOption1: "All",
-            filtreOption2: "Games",
-            filtreOption3: "Web",
-            filtreOption4: "Creation",
-            filtreOption5: "Design",
-          },
-          
-          projetSeul: {
-            p1: "Created by : ", 
-          }
-        },
-      }
-  
-    }
-  
-    // Page des Activités
-    export const texteActivites = {
-  
-      fr: {
-        section1: {
-          h1: "Activités",
-          h2: "offre des événements stimulants, des compétitions captivantes et des opportunités uniques de réseautage.",
-          span: "Notre\u00A0communauté",
-          bouton1: "Voir les activités"
-        },
-  
-        section2: {
-          h2: "Programme d'activités",
-          bouton1: "Plus de détails"
-        }
-      },
-    
-      en: {
-        section1: {
-          h1: "Activities",
-          h2: "offer stimulating events, exciting competitions and unique networking opportunities.",
-          span: "Our\u00A0community",
-          bouton1: "View activities"
-        },
-  
-        section2: {
-          h2: "Activity program",
-          bouton1: "More details"
-        }
-    
-      }
-    
-    }
-  
-  // Page Futur
-  export const texteFutur = {
-  
-      fr: {
-        section1: {
-          titre1: "Futur",
-          p1: "Que vous ayez l'intention de poursuivre des études universitaires ou de vous lancer directement sur le marché du travail, le TIM offre de nombreuses",
-          span1: "opportunités"
-        },
-    
-        section2: {
-            titre1: "Stage de fin d'études",
-            p1: "À la fin de ta formation, tu réaliseras un stage crédité de 8 semaines en entreprise. Ce stage complète ta formation en te permettant d’intégrer et de mettre en application tes différentes compétences acquises dans un environnement professionnel.",
-            titre2: "L'ATE (Alternance Travail-Étude)",
-            p2: "Au cours de ta formation, tu auras la possibilité de réaliser 2 stages optionnels rémunérés en alternance travail-études (ATE) pendant l'été. Tu pourras acquérir une expérience professionnelle dès la première année!"
-        },
-    
-        section3: {
-          titre1: "Perspectives d'emploi",
-          li1: "Intégrateur multimédia",
-          li2: "Concepteur de jeux vidéo",
-          li3: "Animateur 2D / 3D",
-          li4: "Monteur vidéo",
-          li5: "Développeur web",
-          li6: "Et beaucoup d’autres possibilités!"
-        },
-  
-        section4: {
-          titre1: "Études universitaires",
-          a1: "Baccalauréat en médias interactifs à l’UQAM",
-          a2: "Baccalauréat en génie des technologies de l’information à l’ÉTS",
-          a3: "DEC-BAC en création numérique - concentration Création 3D à l’UQAT",
-          a4: "DEC-BAC en création numérique - concentration Cinéma à l’UQAT",
-          a5: "DEC-BAC en création de jeux vidéo - concentration Intégration logicielle à l’UQAT"
-        }
-      
-      },
-  
-      en: {
-        section1: {
-          titre1: "Future",
-          p1: "Whether you're planning to go to university or enter the job market directly, TIM offers a multitude of",
-          span1: "opportunities"
-        },
-    
-        section2: {
-            titre1: "End-of-studies internship",
-            p1: "At the end of your program, you'll complete an 8-week credited internship with a company. This internship completes your training by enabling you to integrate and apply your various acquired skills in a professional environment.",
-            titre2: "The ATE (Work-Study Program)",
-            p2: "During your studies, you'll have the opportunity to complete 2 optional paid work-study internships during the summer. You'll be able to gain professional experience right from your first year!"
-        },
-    
-        section3: {
-          titre1: "Job opportunities",
-          li1: "Multimedia integrator",
-          li2: "Video game designer",
-          li3: "2D / 3D Animator",
-          li4: "Video editor",
-          li5: "Web developer",
-          li6: "And many other possibilities!"
-        },
-  
-        section4: {
-          titre1: "University studies",
-          a1: "Bachelor's degree in interactive media at UQAM",
-          a2: "Bachelor's degree in information technology engineering at ÉTS",
-          a3: "DEC-BAC in Digital Creation - 3D Creation concentration at UQAT",
-          a4: "DEC-BAC in digital creation - concentration in cinema at UQAT",
-          a5: "DEC-BAC in video game creation - concentration in software integration at UQAT"
-        }
+    section2: {
+      p: "Filtrer par : ",
+      filtreOptions: {
+        filtreOption1: "Tout",
+        filtreOption2: "Jeux",
+        filtreOption3: "Web",
+        filtreOption4: "Création",
+        filtreOption5: "Conception",
       }
     }
+  },
   
-  // Page 404 (Page d'erreur)
-  export const texteErreur = {
-    fr: {
-      titre1: "ERREUR 404 - Page non trouvée",
-      message1: "Il me semble que vous vous soyez laissé submerger par toutes sortes d'alternatives..."
+  en: {
+    section1: {
+      titre1: "Student projects",
+      titre2: "Discover the incredible creativity of our students through a showcase of",
+      span: "captivating projects.",
+      titre3: "Explore exceptional achievements and immerse yourself in the world of TIM."
     },
   
-    en: {
-      titre1: "ERROR 404 - Page not found",
-      message1: "It seems that you've let yourself be submerged by all sorts of alternatives..."
+    section2: {
+      p: "Filter by : ",
+      filtreOptions: {
+        filtreOption1: "All",
+        filtreOption2: "Games",
+        filtreOption3: "Web",
+        filtreOption4: "Creation",
+        filtreOption5: "Design",
+      }
     }
   }
+}
+  
+// Page 404 (Page d'erreur)
+export const texteErreur = {
+  fr: {
+    titre: "ERREUR 404 - Page non trouvée",
+    message: "Il me semble que vous vous soyez laissé submerger par toutes sortes d'alternatives..."
+  },
+  
+  en: {
+    titre: "ERROR 404 - Page not found",
+    message: "It seems that you've let yourself be submerged by all sorts of alternatives..."
+  }
+}
