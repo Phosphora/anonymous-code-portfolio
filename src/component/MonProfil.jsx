480a1894eb57912f956bbe5e0bae0d47d5773455
Winnie Li <winnieli535@gmail.com>
2024-01-29 21:47:09 -0500

    Début de la page À propos de moi

--- src/component/MonProfil.jsx
+++ src/component/MonProfil.jsx
@@ -1,16 +1,23 @@
-// Import React and the necessary components
-import React from 'react';
+import '../sass/MonProfil.scss'
+// import '../sass/Carrousel.scss'
+import { texteMonProfil } from '../code/textes';
+import dessinProfil from '../medias/images/static/photo-profil.png'
+// import { scrollUp } from './ScrollToTop';
+// import TechnoIcons from './TechnoIcons';
+// import Carrousel from './Carrousel';
 
-// Define a functional component
-const MonProfil = () => {
-  // JSX code goes here
+export default function MonProfil({langue}) {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-      <p>This is a basic React JSX file.</p>
-    </div>
+    <main className='MonProfil'>
+      {/* SECTION 1 - Introduction */}
+      <section className="introduction">
+        <div className="texte-introduction">
+          <h1>{texteMonProfil[langue].section1.titre}</h1>
+        </div>
+        <div className="dessin-profil">
+          <img src={dessinProfil} alt="Dessin de profil de Winnie Li" />
+        </div>
+      </section>
+    </main>
   );
-};
-
-// Export the component for use in other files
-export default MonProfil;
+}
